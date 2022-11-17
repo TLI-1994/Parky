b3ee81141833919bbd347fe25ac4c2768f967ba0
Tianjiao Li <tl787@cornell.edu>
2022-11-17 17:11:52 -0500

    Initial Commit

---
+++ Parky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parky
+//
+//  Created by 李天骄 on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
